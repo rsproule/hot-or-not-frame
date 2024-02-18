70be23c19e32b82f27165e46c56cc512eb0cf2c4
rsproule <rsproule23@gmail.com>
2024-02-18 14:10:52 -0500

    nit: wording on home

--- app/components/frames/Home.tsx
+++ app/components/frames/Home.tsx
@@ -33,12 +33,16 @@ export async function Home({ state, previousFrame }: FrameContext) {
     >
       <FrameImage>
         <div tw="w-full h-full bg-slate-700 text-white flex flex-col items-center justify-center">
-          <div tw="text-8xl flex mb-10">Top players out of {totalPlayer}:</div>
+          <div tw="text-8xl flex mb-10">
+            Top ranked casters out of {totalPlayer} total players:
+          </div>
           {users
             .sort((a, b) => a.ranking - b.ranking)
             .map((user) => (
               <div key={user.fid} tw="mb-2 text-4xl">
-                {`${user.ranking + 1}: ${user.userDetails.username} - ${user.score}`}
+                {`${user.ranking + 1}: ${user.userDetails.username} - ${
+                  user.score
+                }`}
               </div>
             ))}
         </div>
